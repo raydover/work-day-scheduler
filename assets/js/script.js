113b517852d18847a35591ddc2803661085945dd
raydover <105178034+raydover@users.noreply.github.com>
2022-05-31 23:20:23 -0400

    click listener, text details, save local JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,13 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+// Set click listener for save button, get values of details element and save input text in local storage
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).sibling(".details").val();
+        var time = $(this).parent().attr("id");
 
+        localStorage.setItem(time, text);
+    })
+
+    
